40ab3b6bf83de5b228fd0757d0134753cf491f77
Gab <gabrieltc555@gmail.com>
2024-08-28 20:20:41 -0400

    change import of .env variable

--- src/backend/checkUpdate.ts
+++ src/backend/checkUpdate.ts
@@ -1,7 +1,7 @@
 import { Octokit } from "@octokit/core";
 
 const octokit = new Octokit({
-  auth: "${{ secrets.GITHUB_TOKEN }}",
+  auth: process.env.GITHUB_TOKEN,
 });
 
 export const checkUpdate = async () => {
