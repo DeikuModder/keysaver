e80fe6e7f78c9adcfb150cb22177b229c93be928
Gab <gabrieltc555@gmail.com>
2024-12-03 15:05:09 -0400

    remove manual update, add endpoint to latest.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import AppContent from "./components/AppContent";
-import UPDATE_NOTIFICATION from "./components/Common/UPDATE_NOTIFICATION";
+// import UPDATE_NOTIFICATION from "./components/Common/UPDATE_NOTIFICATION";
 import GeneralProvider from "./providers/GeneralProvider";
 
 function App() {
   return (
     <>
-      <UPDATE_NOTIFICATION />
+      {/* <UPDATE_NOTIFICATION /> */}
       <GeneralProvider>
         <AppContent />
       </GeneralProvider>
